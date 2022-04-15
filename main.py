bfcfa823b1b70d6dd0eaf02f0068197ce379bbf8
Manuverma79 <95175647+Manuverma79@users.noreply.github.com>
2022-04-16 00:32:19 +0530

    ye ye

---
+++ main.py
@@ -0,0 +1,38 @@
+import discord
+
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('we have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    with open("data.txt") as file:
+        data = file.read()
+        yuibite, lorbite = data.split()
+        yuibite = int(yuibite)
+        lorbite = int(lorbite)
+
+    if message.author == client.user:
+        return
+    if message.content.startswith('$cutest'):
+        await message.channel.send("cutest? offcourse its Yuichi's girlfriend lormy")
+    if message.content.startswith('$lorbite'):
+        lorbite = lorbite + 1
+        await message.channel.send(f'lormy bite count = {lorbite}')
+        with open("data.txt", "w") as file:
+            x = str(yuibite) + " " + str(lorbite)
+            file.write(x)
+    if message.content.startswith('$yuibite'):
+        yuibite = yuibite + 1
+        await message.channel.send(f'yuichi bite count = {yuibite}')
+        with open("data.txt", "w") as file:
+            x = str(yuibite) + " " + str(lorbite)
+            file.write(x)
+
+
+client.run("OTU0MDE0NzI4NjkzMzU4Njcy.YjM9dA.Gzk3AKALdKHASsCP9MHXmwYbwGs")
